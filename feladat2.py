fd0e8e3c0be4d6e15cbef1035b43c8f337ee8c80
mercijonas <36500994+mercijonas@users.noreply.github.com>
2018-03-08 19:47:04 +0100

    Add files via upload
    Feltöltött feladatok:8,9,10,11,18

--- feladat2.py
+++ feladat2.py
@@ -20,3 +20,86 @@ def main():
     print(feladat_3(513))
 if __name__=='__main__':
     main()
+
+def feladat_8(n):
+    osszeg=0
+    db=0
+    for i in range(1,n):
+        osszeg+=i
+        db+=1
+        if osszeg>=n:
+            return db
+def main():
+    print(feladat_8(16))
+if __name__=='__main__':
+    main()
+
+def feladat_9():
+    magassag=1
+    perc=0
+    while magassag<=300:
+        k=2
+        magassag=magassag+(1/k)
+        k+=1
+        perc+=1
+    return perc
+def main():
+    print(feladat_9())
+if __name__ == '__main__':
+    main()
+
+def feladat_10():
+    try:
+        fajl=open("be.txt",mode="r")
+        maxi=0
+        for sor in fajl:
+            sor=sor.strip()
+            if (sor[0].isupper() and len(sor)>maxi):
+                    maxi=len(sor)
+        return maxi
+    except Exception as e:
+        print(e)
+    fajl.close()
+
+def main():
+    print(feladat_10())
+if __name__ == '__main__':
+    main()
+
+def feladat_11():
+    try:
+        fajl=open("be.txt",mode="r")
+        mini=10000
+        lst="aábccsddzdzseéfggyhiíjkllymnnyoóöőpqrsszttyuúüűvwxyzzs"
+        for sor in fajl:
+            sor=sor.strip()
+            if sor[-1] not in lst and len(sor)<mini:
+                    mini=len(sor)
+        return mini
+    except Exception as e:
+        print(e)
+    fajl.close()
+
+def main():
+    print(feladat_11())
+if __name__ == '__main__':
+    main()
+
+def feladat_18():
+    try:
+        fajl1=open("be.txt",mode="r")
+        for sor in fajl1:
+            ls=sor.split(" ")
+            pontok=ls[-1].split(":")
+            if int(pontok[0])>int(pontok[1]):
+                print(ls[0])
+            else:
+                print(ls[2])
+    except Exception as e:
+        print(e)
+    fajl1.close()
+def main():
+    feladat_18()
+if __name__ == '__main__':
+    main()
+
